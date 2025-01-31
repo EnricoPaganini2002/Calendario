530b279c27ec6f3e6c8033d7d5fdabc1373d78e1
EnricoPaganini2002 <enpaganini418@gmail.com>
2025-01-30 23:32:03 -0300

    git

--- obj/Debug/net9.0/Calendario.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calendario.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calendario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c723a89ec7b1fcf944cd8ae69662a19ef7dbec3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calendario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calendario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
