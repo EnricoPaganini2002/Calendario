66118067e6e36a519f59547eadb343f8a650f50a
EnricoPaganini2002 <enpaganini418@gmail.com>
2025-02-04 20:24:37 -0300

    Funcionalidad modal

--- obj/Debug/net9.0/Calendario.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calendario.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calendario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0404363bd80d055a98bfdfc55ca4aecd6df4533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe0b184e3e410a78e6857f08da0d7bed34f7a52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calendario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calendario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
